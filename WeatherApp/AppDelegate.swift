bddae65880575f47ea7a7c92691c950bbbaed521
Stephen Jayakar <ajay@Stephens-MacBook-Pro.local>
2017-10-09 23:37:25 -0700

    basic pod functionality

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -8,23 +8,39 @@
 
 import UIKit
 import Alamofire
-
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    // api key e992c804052acdd34db963b614a1b985
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        // Get current location
+        let locationManager = CLLocationManager()
+        // HOW DO WE GET LOCATION IF WE CAN'T REQUEST PERMISSION?
+        // Request weather information
+        // TODO: Unhardcode from location
+        var weatherData = Alamofire.request("https://api.darksky.net/forecast/e992c804052acdd34db963b614a1b985/37.8267,-122.4233").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        // Send Notification!
+        let notification = NotificationCenter.default
+        notification.post(name: NSNotification.Name("meow"), object: "test")
         return true
     }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        // Fetch the weather data!
-        // Post a notification!
-    }
+
+
 }
 
---
+++ WeatherApp/View/WeatherViewController.swift
@@ -0,0 +1,56 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Stephen Jayakar on 10/9/17.
+//  Copyright © 2017 Stephen Jayakar. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    var temperatureLabel: UILabel!
+    var rainLabel: UILabel!
+    var weatherDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNotificationCenter()
+        setupText()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Setup Functions
+    func setupNotificationCenter() {
+        let nc: NotificationCenter = NotificationCenter.default
+        // nc.addObserver(self, selector: #selector(weatherInfoReceived(x)), name: NSNotification.Name(rawValue: "meow"), object: x)
+    }
+    
+    func setupText() {
+        temperatureLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 50, height: 40))
+        temperatureLabel.text = "200"
+        temperatureLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(temperatureLabel)
+        
+        rainLabel = UILabel(frame: CGRect(x: 10, y: 50, width: 50, height: 40))
+        rainLabel.text = "ITS FUCKING RAINING"
+        rainLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(rainLabel)
+        
+        weatherDescription = UILabel(frame: CGRect(x: 10, y: 90, width: 50, height: 40))
+        weatherDescription.text = "Fuck the weather"
+        view.addSubview(weatherDescription)
+    }
+    
+    // Weather notification received
+    func weatherInfoReceived(_ notification: Notification) {
+        print(notification.object as! String)
+        print("data received")
+    }
+}
+
