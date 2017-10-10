8bb7361f8bda60dc275ae73c207236b53ab85333
Stephen Jayakar <ajay@Stephens-MacBook-Pro.local>
2017-10-10 00:43:50 -0700

    json stuff

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -22,19 +22,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // HOW DO WE GET LOCATION IF WE CAN'T REQUEST PERMISSION?
         // Request weather information
         // TODO: Unhardcode from location
-        var weatherData = Alamofire.request("https://api.darksky.net/forecast/e992c804052acdd34db963b614a1b985/37.8267,-122.4233").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-            }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
-            }
-        }
+//        var weatherData = Alamofire.request("https://api.darksky.net/forecast/e992c804052acdd34db963b614a1b985/37.8267,-122.4233").responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+//
+//            if let json = response.result.value {
+//                print("JSON: \(json)") // serialized json response
+//            }
+//
+//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+//                print("Data: \(utf8Text)") // original server data as UTF8 string
+//            }
+//        }
         // Send Notification!
         let notification = NotificationCenter.default
         notification.post(name: NSNotification.Name("meow"), object: "test")
--- WeatherApp/Controller/SplashViewController.swift
+++ WeatherApp/Controller/SplashViewController.swift
@@ -15,7 +15,8 @@ class SplashViewController: UIViewController {
     
     var current_desc: String!
     var minutely_desc: String!
-    var rain_time: String!
+    var precip: Int!
+    var rain_time: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +24,23 @@ class SplashViewController: UIViewController {
         // needs to segue after like a second
         
         var weatherData = Alamofire.request("https://api.darksky.net/forecast/e992c804052acdd34db963b614a1b985/37.8267,-122.4233").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
             
-            if let json = JSON(response.result.value) {
+            if let json = response.result.value {
                 //print("JSON: \(json)") // serialized json response
+                let json2 = JSON(json)
                 
-                current_desc = json["currently"]["summary"].stringValue
-                minutely_desc = json["minutely"]["summary"].stringValue
-                rain_time = json["minutely"]["data"]["time"]
+                self.current_desc = json2["currently"]["summary"].stringValue
+                self.minutely_desc = json2["minutely"]["summary"].stringValue
+                self.precip = json2["minutely"]["data"]["precipProbability"].int
+                self.rain_time = json2["minutely"]["data"]["time"].int
                 
-                print(current_desc)
-                print(minutely_desc)
-                print(rain_time)
-                
-            }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
+                print(self.current_desc)
+                print(self.minutely_desc)
+                print(self.precip)
+                print(self.rain_time)
             }
         }
         
